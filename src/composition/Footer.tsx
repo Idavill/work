36dc0c27ccdb10662bdd13773791b6c67637ee35
Idavill <idamariavilladsen@gmail.com>
2025-10-15 09:28:16 +0200

    remove placeholder text

--- src/composition/Footer.tsx
+++ src/composition/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer({selectedProject}:FooterProps) {
     <>
     {!selectedProject && (
         <footer className="bg-gray-900 text-center text-white p-4 text-sm z-3">
-          &copy; {new Date().getFullYear()} Your Name. All rights reserved.
+          &copy; {new Date().getFullYear()} All rights reserved.
         </footer>
       )
     }
